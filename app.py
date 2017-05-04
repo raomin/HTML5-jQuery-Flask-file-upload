65cd4d448b18e1dec2ba747c46e85131e34d8f9c
Romain Bourgue <Romain Bourgue>
2017-05-05 00:41:36 +0200

    support uppercase extension

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def upload():
             return jsonify({"success":True})
 
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 if __name__ == "__main__":
     app.run(debug=True)
